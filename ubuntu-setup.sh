6dbda78e22e82539a37c57d45585531adb095877
JacobDawson98 <dawson@iastate.edu>
2019-02-02 00:07:50 -0600

    Feature: Add ubuntu setup

---
+++ ubuntu-setup.sh
@@ -0,0 +1,166 @@
+#!/bin/sh
+
+
+echo "Ubuntu Install Setup Script"
+
+# Some configs reused from:
+# https://github.com/startcode/autosetup/blob/master/ubuntu_install_common_packages.sh#L8
+
+echo ""
+cecho "###############################################" $red
+cecho "#        DO NOT RUN THIS SCRIPT BLINDLY       #" $red
+cecho "#         YOU'LL PROBABLY REGRET IT...        #" $red
+cecho "#                                             #" $red
+cecho "#              READ IT THOROUGHLY             #" $red
+cecho "#         AND EDIT TO SUIT YOUR NEEDS         #" $red
+cecho "###############################################" $red
+echo ""
+
+# Set continue to false by default.
+CONTINUE=false
+
+echo ""
+cecho "Have you read through the script you're about to run and " $red
+cecho "understood that it will make changes to your computer? (y/n)" $red
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  CONTINUE=true
+fi
+
+if ! $CONTINUE; then
+  # Check if we're continuing and output a message if not
+  cecho "Please go read the script, it only takes a few minutes" $red
+  exit
+fi
+
+# Here we go.. ask for the administrator password upfront and run a
+# keep-alive to update existing `sudo` time stamp until script has finished
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+#############################################
+### Generate ssh keys & add to ssh-agent
+### See: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+#############################################
+
+echo "Generating ssh keys, adding to ssh-agent..."
+read -p 'Input email for ssh key: ' useremail
+
+echo "Use default ssh file location, enter a passphrase: "
+ssh-keygen -t rsa -b 4096 -C "$useremail"  # will prompt for password
+eval "$(ssh-agent -s)"
+
+# Now that sshconfig is synced add key to ssh-agent and
+# store passphrase in keychain
+ssh-add -K ~/.ssh/id_rsa
+
+# If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain.
+
+if [ -e ~/.ssh/config ]
+then
+    echo "ssh config already exists. Skipping adding osx specific settings... "
+else
+	echo "Writing osx specific settings to ssh config... "
+   cat <<EOT >> ~/.ssh/config
+	Host *
+		AddKeysToAgent yes
+		UseKeychain yes
+		IdentityFile ~/.ssh/id_rsa
+EOT
+fi
+
+#############################################
+### Add ssh-key to GitHub via api
+#############################################
+
+echo "Adding ssh-key to GitHub (via api)..."
+echo "Important! For this step, use a github personal token with the admin:public_key permission."
+echo "If you don't have one, create it here: https://github.com/settings/tokens/new"
+
+retries=3
+SSH_KEY=`cat ~/.ssh/id_rsa.pub`
+
+for ((i=0; i<retries; i++)); do
+      read -p 'GitHub username: ' ghusername
+      read -p 'Machine name: ' ghtitle
+      read -sp 'GitHub personal token: ' ghtoken
+
+      gh_status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -u "$ghusername:$ghtoken" -d '{"title":"'$ghtitle'","key":"'"$SSH_KEY"'"}' 'https://api.github.com/user/keys')
+
+      if (( $gh_status_code -eq == 201))
+      then
+          echo "GitHub ssh key added successfully!"
+          break
+      else
+			echo "Something went wrong. Enter your credentials and try again..."
+     		echo -n "Status code returned: "
+     		echo $gh_status_code
+      fi
+done
+
+[[ $retries -eq i ]] && echo "Adding ssh-key to GitHub failed! Try again later."
+
+
+##############################
+# Prepare Third Party Repos  #
+##############################
+
+# google-chrome-stable
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# steam
+add-apt-repository multiverse
+
+
+##############################
+# Install Packages           #
+##############################
+
+echo "Starting packages install"
+
+apt-get update
+apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+packages=(
+ack-grep
+apt-file
+aptitude
+git
+google-chrome-stable
+htop
+libgconf-2-4
+libappindicator1
+steam
+python-pip
+python3-pip
+vlc
+xclip
+)
+
+for p in ${packages[*]}; do
+    echo $p
+    apt-get install -y $p
+done
+
+apt-get -f install
+apt-get autoremove -y
+apt-get clean -y
+
+# Install discord
+cd ~/Downloads
+wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
+dpkg -i discord-0.0.1.deb
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+#############################################
+### Install dotfiles repo, run link script  #
+#############################################
+git clone https://github.com/mattjmorrison/dotfiles.git ~/dotfiles
+cd ~/dotfiles/install-scripts
+bash Linux/install-packages.sh
+bash Linux/create-symlinks.sh
